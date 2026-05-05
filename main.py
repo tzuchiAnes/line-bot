7156003b84d80ae879b6fb9ca28a4518c5bd2a7b
tzuchiAnes <tzuchi.anes@gmail.com>
2026-05-05 14:01:03 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,46 @@
+from fastapi import FastAPI, Request, Header
+
+from linebot import LineBotApi, WebhookHandler
+
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+
+import os
+
+app = FastAPI()
+
+LINE_CHANNEL_ACCESS_TOKEN = os.getenv("LINE_CHANNEL_ACCESS_TOKEN")
+
+LINE_CHANNEL_SECRET = os.getenv("LINE_CHANNEL_SECRET")
+
+line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
+
+handler = WebhookHandler(LINE_CHANNEL_SECRET)
+
+@app.post("/callback")
+
+async def callback(request: Request, x_line_signature: str = Header(...)):
+
+    body = await request.body()
+
+    body = body.decode("utf-8")
+
+    handler.handle(body, x_line_signature)
+
+    return "OK"
+
+@handler.add(MessageEvent, message=TextMessage)
+
+def handle_message(event):
+
+    user_msg = event.message.text
+
+    reply = f"你說的是：{user_msg}"
+
+    line_bot_api.reply_message(
+
+        event.reply_token,
+
+        TextSendMessage(text=reply)
+
+    )
+ 
